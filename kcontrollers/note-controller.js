d63f3396450bb105e1564f979b12b65b72ca587a
david-mears <kieukmail@gmail.com>
2019-02-23 18:08:41 +0000

    Single notes pages load the correct note text. A global variable is keeping track of all the notes - this is bad practice.

---
+++ kcontrollers/note-controller.js
@@ -0,0 +1,44 @@
+(function(exports) {
+
+    changeHelloToHowdy = function() {
+      var element = document.getElementById('app');
+      element.innerHTML = 'howdy';
+    };
+
+    function NoteController(noteList) {
+        this.noteList = noteList;
+        this.noteList.addMessage('Favourite drink: seltzer')
+        this.noteListView = new NoteListView(this.noteList)
+    }
+
+    NoteController.prototype.addHTMLtoAppElement = function() {
+        var app = document.getElementById('app')
+        app.innerHTML = this.noteListView.makeView()
+    }
+
+    function getNoteTextById(id) {
+      var i;
+      for (i = 0; i < allNotes.length; i++) {
+          if (allNotes[i].id == id) {
+              return allNotes[i].text
+          }
+      }
+      return 'Note not found.'
+    }
+
+    function showRelevantNote() {
+        var hashid = window.location.hash.slice(7)
+        var element = document.getElementById('app');
+        element.innerHTML = 'Note' + hashid + ': ' + getNoteTextById(hashid);
+        console.log('got here')
+    }
+
+    function makeUrlChangeShowRelevantNote() {
+        window.addEventListener("hashchange", showRelevantNote);
+    };
+
+    makeUrlChangeShowRelevantNote();
+
+    exports.NoteController = NoteController
+
+})(this)
--- pmodels/note.js
+++ pmodels/note.js
@@ -3,11 +3,13 @@
 (function(exports) {
 
     var counter = 0;
+    var allNotes = [];
 
     function Note(message='This is a message') {
         counter += 1;
         this.text = message;
         this.id = counter;
+        allNotes.push(this);
     }
 
     Note.prototype.read = function() {
@@ -15,4 +17,5 @@
     }
 
     exports.Note = Note
+    exports.allNotes = allNotes
 })(this)
